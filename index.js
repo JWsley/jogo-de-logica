c38440bf09601220fa897142c50b24546c098781
“JWsley” <jonathanwleycijani@gmail.com>
2022-10-17 00:51:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// const button = document.querySelector('button')
+// button.addEventListener('click',function(){
+//     const audio = document.querySelector('#audio1');
+//     audio.play()
+// })
+
+
+
+// const title = document.querySelector('h1')
+// button.addEventListener('click',function(){
+//     const audio = document.querySelector('audio');
+//     audio.play()
+// })
+
+
+function Rapaz(){
+    var audio = document.querySelector('#rapaz')
+    audio.play()
+}
+
+function uii(){
+    var audio = document.querySelector('#uiii')
+    audio.play()
+}
+
+
+function combado(){
+    var audio = document.querySelector('#combom')
+    audio.play()
+}
